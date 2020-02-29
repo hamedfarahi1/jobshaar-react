7c5c0ddd49fa639427c53e734485e133d1951e79
hamedfarahikharazmi@gmail.com <hamedfarahikharazmi@gmail.com>
2020-02-29 22:49:56 +0330

    + define error messages

--- src/core/interceptors/errorHandlerInterceptor.js
+++ src/core/interceptors/errorHandlerInterceptor.js
@@ -5,17 +5,29 @@ import { store } from '../_helpers';
 export const errorHandlerInterceptor = (error) => {
 
 	const statusCode = error.response.status;
-	switch (statusCode) {
-		case 504:
-			errorMessage("خطا در اتصال به سرور")
-			break;
-		default:
-			errorMessage("خطا")
-
-	}
+	createErrorMessage(statusCode);
 	return Promise.reject({ ...error })
 }
 
-const errorMessage = (message) => {
+const createErrorMessage = (statusCode) => {
+	switch (statusCode) {
+		case 504:
+			showErrorMessage("خطا در اتصال به سرور");
+			break;
+		case 601:
+			showErrorMessage("پسوورد اشتباه وارد شده است");
+			break;
+		case 604:
+			showErrorMessage("نام کاربری نا معتبر است");
+			break;
+		case 401:
+			showErrorMessage("احراز هویت با موفقیت انجام نشد");
+			break;
+		default:
+			showErrorMessage("خطا");
+	}
+}
+
+const showErrorMessage = (message) => {
 	store.dispatch(uiActions.errorSnackbar(message))
 }
--- src/shared/component/snackbar/MySnackbar.jsx
+++ src/shared/component/snackbar/MySnackbar.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { useSelector } from "react-redux";
 import Snackbar from "@material-ui/core/Snackbar";
-import IconButton from "@material-ui/core/IconButton";
-import { Icon } from "@material-ui/core";
 import { uiActions } from "../../../core/_actions/ui.actions";
 import { store } from '../../../core/_helpers';
 import MuiAlert from '@material-ui/lab/Alert';
