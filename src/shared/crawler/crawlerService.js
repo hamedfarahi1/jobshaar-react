12a38d05421a2d6ac04e7a5884202cb069418896
hamedfarahikharazmi@gmail.com <hamedfarahikharazmi@gmail.com>
2020-04-18 21:14:17 +0430

    + change baseUrl

--- src/shared/crawler/crawlerService.js
+++ src/shared/crawler/crawlerService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:3001',
+	baseURL: 'http://188.40.195.134:3000',
 	/* other custom settings */
 });
 
